57dab64a6df1ee7fa53f2fce231ad3ae55df3bda
Lucas F. F. Lopes <34213192+lucasfarias300@users.noreply.github.com>
2022-05-18 09:23:51 -0700

    Updates the existing code "to use only lower case for variables names. (page 10)" and creates a python file for the second chapter

--- 01/basicCommands.py
+++ 01/basicCommands.py
@@ -1,12 +1,12 @@
 def totalSeconds(hours, minutes, seconds):
-    hoursInSeconds = hours * 60 * 60
-    minutesInSeconds = minutes * 60
-    return hoursInSeconds + minutesInSeconds + seconds
+    hours_in_seconds = hours * 60 * 60
+    minutes_in_seconds = minutes * 60
+    return hours_in_seconds + minutes_in_seconds + seconds
 
 
 def kilometerToMile(kilometers):
-    oneMileInKilometers = 1.61
-    return kilometers / oneMileInKilometers
+    one_mile_in_kilometers = 1.61
+    return kilometers / one_mile_in_kilometers
 
 
 def secondsToMinutesAndSeconds(totalSeconds):
@@ -29,29 +29,30 @@ def main():
 
     # questions that need code
     print("Number 1\nHow many seconds are there in 42 minutes 42 seconds?")
-    formatSeconds = "{:,.2f}".format(totalSeconds(0, 42, 42))
-    print("The answer is " + formatSeconds + " seconds")
+    format_seconds = "{:,.2f}".format(totalSeconds(0, 42, 42))
+    print("The answer is " + format_seconds + " seconds")
     print("\n")
 
     print("Number 2\nHow many miles are there in 10 kilometers?")
     # Hint: there are 1.61 kilometers in a mile
-    formatTotalMiles = "{:,.2f}".format(kilometerToMile(10))
+    format_total_miles = "{:,.2f}".format(kilometerToMile(10))
     #formatKilometers = "{:,.2f}".format(kilometers)
-    print("There is " + formatTotalMiles + " miles in " +
+    print("There is " + format_total_miles + " miles in " +
           "{:,.2f}".format(10) + " kilometers")
     print("\n")
 
     print("Number 3\nIf you run a 10 kilometer race in 42 minutes 42 seconds, \n what is your average pace (time per mile in minutes and seconds)?\nWhat is your average speed in miles per hour?")
     miles = kilometerToMile(10)
-    paceInSeconds = totalSeconds(0, 42, 42)
-    paceInMinutesAndSeconds = "The 'raw' pace is " + \
-        secondsToMinutesAndSeconds(paceInSeconds) + \
+    pace_in_seconds = totalSeconds(0, 42, 42)
+    pace_in_minutes_and_seconds = "The 'raw' pace is " + \
+        secondsToMinutesAndSeconds(pace_in_seconds) + \
         " per " + "{:,.2f}".format(miles) + " miles"
-    print(paceInMinutesAndSeconds)
+    print(pace_in_minutes_and_seconds)
     # 42'42'' --- 6.21
     #  3,600  --- x
-    paceInMinutesAndSeconds = miles * 3600 / paceInSeconds
-    print("Your pace is " + "{:.2f}".format(paceInMinutesAndSeconds) + " mi/h")
+    pace_in_minutes_and_seconds = miles * 3600 / pace_in_seconds
+    print("Your pace is " +
+          "{:.2f}".format(pace_in_minutes_and_seconds) + " mi/h")
     print("\n")
 
 
---
+++ 02/variablesExpressionsAndStatements.py
@@ -0,0 +1,1 @@
+# It is legal to use uppercase letters, but it is conventional to use only lower case for variables names. (page 10)
